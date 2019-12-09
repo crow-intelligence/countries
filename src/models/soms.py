fd5b88a19a6c7c318b0d49fcc001cc33541cbd92
zolizoli <zoltan.varju@gmail.com>
2019-12-09 15:37:04 +0100

    20x20 som

--- src/models/soms.py
+++ src/models/soms.py
@@ -1,5 +1,7 @@
+import pickle
 import pandas as pd
 import SimpSOM as sps
+from PIL import Image, ImageChops
 
 
 df = pd.read_csv("data/processed/final.csv",
@@ -8,11 +10,30 @@ labels = df["Country"]
 df = df.drop(["Country", "Aggregated", "Average"], axis=1)
 
 net = sps.somNet(20, 20, df.values, PBC=True, PCI=True)
+#net = sps.somNet(40, 40, df.values, PBC=True, PCI=True, loadFile="somweights")
 net.train(0.1, 10000)
 net.save('somweights')
-net.nodes_graph(colnum=6)
-net.diff_graph()
+# net.nodes_graph(colnum=6)
+# net.diff_graph()
 
 net.cluster(df.values, type='qthresh')
-#net.diff_graph(show=True, printout=True)
+net.diff_graph(show=True, printout=True)
 
+
+# Here we first define a few useful functions
+def autocrop(fileName):
+    im = Image.open(fileName)
+    im = im.crop((0, 100, 2900, im.size[1]))
+    bg = Image.new(im.mode, im.size, im.getpixel((0, 0)))
+    diff = ImageChops.difference(im, bg)
+    diff = ImageChops.add(diff, diff, 2.0, -100)
+    bbox = diff.getbbox()
+    if bbox:
+        return im.crop(bbox)
+
+projData = net.project(df.values)
+cropped = autocrop('nodesDifference.png')
+cropped.save('cropped.png')
+
+with open("projData.pkl", "wb") as f:
+    pickle.dump(projData, f)
---
+++ src/visualization/somviz.py
@@ -0,0 +1,116 @@
+import pickle
+
+import numpy as np
+import pandas as pd
+import plotly.graph_objs as go
+import plotly.offline as py
+
+with open("projData.pkl", "rb") as f:
+    projdata = pickle._load(f)
+
+
+df = pd.read_csv("data/processed/final.csv",
+                 sep=",")
+
+labels = list(df["Country"])
+country_cluster = {}
+i = 0
+with open("qthresh_clusters.dat", "r") as f:
+    for l in f:
+        l = l.strip().split()
+        l = [int(e) for e in l]
+        countries = [labels[i] for i in l]
+        for country in countries:
+            country_cluster[country] = str(i)
+        i += 1
+
+clusters = [country_cluster[country] for country in labels]
+df["Clusters"] = clusters
+
+all_write = df.to_csv(index=False)
+with open("data/processed/final_with_clusters.csv", "w") as f:
+    f.write(all_write)
+x_coords = [x for x,y in projdata]
+y_coords = [y for x,y in projdata]
+
+comp = list(df["Competitiveness"])
+gdp = list(df["GDP"])
+business = list(df["Business"])
+law = list(df["Law"])
+science = list(df["Science"])
+happiness = list(df["Happiness"])
+freedom = list(df["Freedom"])
+hdi = list(df["HDI"])
+avg = list(df["Average"])
+aggregated = list(df["Aggregated"])
+
+header_labels = list(df.columns)
+h = ["Country", "Competitiveness", "GDP", "Business", "Law", "Science",
+     "Happiness", "Freedom", "HDI", "Average", "Aggregated", "Cluster"]
+ranks = list(zip(labels, comp, gdp, business, law, science, happiness,
+                 freedom, hdi, avg, aggregated, clusters))
+
+legends = []
+for i in range(len(ranks)):
+    data = ranks[i]
+    text = list(zip(h, data))
+    text = [(str(e[0] + ":"), str(e[1])) for e in text]
+    text = [" ".join(e) for e in text]
+    text = "<br>".join(text)
+    legends.append(text)
+
+###############################################################################
+###  Source: https://www.kaggle.com/asparago/unsupervised-learning-with-som  ##
+###############################################################################
+trace0 = go.Scatter(
+    x=x_coords,
+    y=y_coords,
+    hoveron=labels,
+    text = legends,
+    mode = 'markers',
+    marker = dict(
+        size = 8,
+        color = clusters,
+        colorscale ='Jet',
+        showscale = False,
+        opacity = 1
+    ),
+    showlegend = False
+
+)
+data = [trace0]
+
+layout = go.Layout(
+    images= [dict(
+                  source= "cropped.png",
+                  xref="x",
+                  yref="y",
+                  x=-0.5,
+                  y=20.2*2/np.sqrt(3)*3/4,
+                  sizex= 43.5,
+                  sizey= 22*2/np.sqrt(3)*3/4,
+                  sizing="stretch",
+                  opacity=0.8,
+                  layer="below")],
+    width = 800,
+    height = 800,
+    hovermode= 'closest',
+    xaxis= dict(
+        range=[-1,21],
+        zeroline=False,
+        showgrid=False,
+        ticks='',
+        showticklabels=False
+    ),
+    yaxis=dict(
+        range=[-1,21],
+        zeroline=False,
+        showgrid=False,
+        ticks='',
+        showticklabels=False
+    ),
+    showlegend= True
+)
+
+fig = dict(data=data, layout=layout)
+py.plot(fig, filename='styled-scatter.html')
